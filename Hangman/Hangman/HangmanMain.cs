f3463370cdeecc8028bcba743bcab1944d23139c
Dan French <dan.french82@gmail.com>
2019-01-10 23:58:23 +0000

    Initial Tests created and associated methods.

---
+++ Hangman/Hangman/HangmanMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class HangmanMain
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string> { "big", "car", "lorry" };
+            string word = SelectRandomWord(words);
+        }
+
+        /// <summary>
+        /// Selects a random word from a list of words
+        /// </summary>
+        /// <param name="words">List of words</param>
+        /// <returns>string</returns>
+        public static string SelectRandomWord(List<string> words)
+        {
+            Random random = new Random();
+            return words[random.Next(0, words.Count())];
+        }
+
+        /// <summary>
+        /// Generates a word string with correct guesses added
+        /// </summary>
+        /// <param name="word">the hidden word</param>
+        /// <param name="guesses">list of guesses made</param>
+        /// <returns>string</returns>
+        public static string GenerateWordString(string word, List<string> guesses)
+        {
+            string newWord = ""; 
+            foreach (char letter in word)
+            {
+                if (guesses.Contains(letter.ToString()))
+                    newWord += letter;
+                else
+                    newWord += "_";
+            }
+            return newWord;
+        }
+
+        /// <summary>
+        /// Adds a guess(string) to a list of guesses
+        /// </summary>
+        /// <param name="guesses">List of guesses</param>
+        /// <param name="guess">New guess</param>
+        /// <returns></returns>
+        public static List<string> AddToGuesses(List<string> guesses, string guess)
+        {
+            guesses.Add(guess);
+            return guesses;
+        }
+    }
+}
---
+++ Hangman/HangmanTests/HangmanTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hangman;
+using System.Collections.Generic;
+
+namespace HangmanTests
+{
+    [TestClass]
+    public class HangmanTest
+    {
+        [TestMethod]
+        public void GenerateWord_returns_string()
+        {
+            List<string> words = new List<string> { "big" , "car" , "lorry" };
+            Assert.IsNotNull(HangmanMain.SelectRandomWord(words));
+        }
+
+        [TestMethod]
+        public void GetWord_returns_correct_format()
+        {
+            string word = "abc" ;
+            List<string> guesses = new List<string> { "d" };
+
+            Assert.AreEqual(HangmanMain.GenerateWordString(word, guesses), "___");
+        }
+
+        [TestMethod]
+        public void GetWord_returns_correct_format_with_1_guess()
+        {
+            string word = "abc";
+            List<string> guesses = new List<string> { "a" };
+
+            Assert.AreEqual(HangmanMain.GenerateWordString(word, guesses), "a__");
+        }
+
+        [TestMethod]
+        public void GetWord_returns_correct_format_with_2_guess()
+        {
+            string word = "abc";
+            List<string> guesses = new List<string> { "a" , "c" };
+
+            Assert.AreEqual(HangmanMain.GenerateWordString(word, guesses), "a_c");
+        }
+
+        [TestMethod]
+        public void GetWord_returns_correct_format_with_incorrect_guess()
+        {
+            string word = "abc";
+            List<string> guesses = new List<string> { "d" , "a" };
+
+            Assert.AreEqual(HangmanMain.GenerateWordString(word, guesses), "a__");
+        }
+
+        [TestMethod]
+        public void AddToGuesses_increases_list_items()
+        {
+            List<string> guesses = new List<string> { };
+            string guess = "a";
+
+            guesses = HangmanMain.AddToGuesses(guesses, guess);
+
+            Assert.AreEqual(guesses.Count, 1);
+        }
+    }
+}

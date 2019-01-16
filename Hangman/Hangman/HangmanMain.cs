e8d188eb898a1e4910f90f160f5e8e8c1e2b18ac
Dan French <dan.french82@gmail.com>
2019-01-16 21:58:59 +0000

    Basic game structure complete. No printed representation of the hangman at present.

--- Hangman/Hangman/HangmanMain.cs
+++ Hangman/Hangman/HangmanMain.cs
@@ -11,27 +11,15 @@ namespace Hangman
         static void Main(string[] args)
         {
             HangmanMain hm = new HangmanMain();
-            List<string> words = new List<string> { "big", "car", "lorry" };
-            string word = hm.SelectRandomWord(words);
+            hm.PlayGame();
         }
 
-        /// <summary>
-        /// Selects a random word from a list of words
-        /// </summary>
-        /// <param name="words">List of words</param>
-        /// <returns>string</returns>
         public string SelectRandomWord(List<string> words)
         {
             Random random = new Random();
             return words[random.Next(0, words.Count())];
         }
 
-        /// <summary>
-        /// Generates a word string with correct guesses added
-        /// </summary>
-        /// <param name="word">the hidden word</param>
-        /// <param name="guesses">list of guesses made</param>
-        /// <returns>string</returns>
         public string GenerateWordString(string word, List<string> guesses)
         {
             string newWord = ""; 
@@ -45,26 +33,90 @@ namespace Hangman
             return newWord;
         }
 
-        /// <summary>
-        /// Adds a guess(string) to a list of guesses
-        /// </summary>
-        /// <param name="guesses">List of guesses</param>
-        /// <param name="guess">New guess</param>
-        /// <returns></returns>
-        public List<string> AddToGuesses(List<string> guesses, string guess)
+        public List<string> AddStringToList(List<string> guesses, string guess)
         {
             guesses.Add(guess);
             return guesses;
         }
 
-        public int CalcLives(string word, List<string> guesses, int lives)
+        public bool RemoveLife(string word, string guess)
         {
-            foreach (var letter in guesses)
+            return (!word.Contains(guess));
+        }
+
+        public bool OutOfLives(int lives)
+        {
+            return (lives == 0);
+        }
+
+        public bool WordGuessed(string word, List<string> guesses)
+        {
+            return (GenerateWordString(word, guesses) == word);
+        }
+
+        public bool RoundNotFinished(string word, List<string> guesses, int lives)
+        {
+            return (!WordGuessed(word, guesses) && !OutOfLives(lives));
+        }
+
+        public string FormatGuessesForPrinting(List<string> guesses)
+        {
+            string formattedString = "";
+            foreach (var guess in guesses)
+                formattedString += guess;
+            return formattedString;
+        }
+
+        public string EndMessage(string word, List<string>guesses, int lives)
+        {
+            string output = "";
+            if (OutOfLives(lives))
+                output += "You lose";
+            else
+                output += string.Format("Well done! You have guessed the word {0}", GenerateWordString(word, guesses));
+            return output;
+        }
+
+        public bool DuplicateGuess(List<string> guesses, string guess)
+        {
+            return (guesses.Contains(guess));
+        }
+
+        public string OutputGuessRequest()
+        {
+            Console.WriteLine("Please enter a guess: ");
+            return Console.ReadLine();
+        }
+
+        public void PlayGame()
+        {
+            bool cont = true;
+            while (cont == true)
             {
-                if (!word.Contains(letter))
-                    lives -= 1;   
+                int lives = 3;
+                List<string> guesses = new List<string>();
+                List<string> words = new List<string> { "elephant", "cheese", "rhythm", "temperature" };
+                string word = SelectRandomWord(words);
+                while (RoundNotFinished(word, guesses, lives))
+                {
+                    Console.WriteLine(GenerateWordString(word, guesses));
+                    Console.WriteLine(string.Format("Current guesses: {0}", FormatGuessesForPrinting(guesses)));
+                    string guess = OutputGuessRequest();
+                    while (DuplicateGuess(guesses, guess))
+                    {
+                        Console.WriteLine("Duplicate");
+                        guess = OutputGuessRequest();
+                    }
+                    AddStringToList(guesses, guess);
+                    if (RemoveLife(word, guess))
+                        lives -= 1;
+                }
+                Console.WriteLine(EndMessage(word, guesses, lives));
+                Console.WriteLine("");
+                Console.WriteLine("Play again? Y/N");
+                if (Console.ReadLine().ToLower() == "n")
+                    cont = false;
             }
-            return lives;
         }
     }
 }
--- Hangman/HangmanTests/HangmanTest.cs
+++ Hangman/HangmanTests/HangmanTest.cs
@@ -14,7 +14,7 @@ namespace HangmanTests
         }
 
         [TestMethod]
-        public void GenerateWord_ReturnsString()
+        public void SelectRandomWord_ReturnsString()
         {
             HangmanMain hm = MakeHangman();
             List<string> words = new List<string> { "big" , "car" , "lorry" };
@@ -63,48 +63,155 @@ namespace HangmanTests
         }
 
         [TestMethod]
-        public void AddToGuesses_IncreasesListItems()
+        public void AddStringToList_IncreasesListItems()
         {
             HangmanMain hm = MakeHangman();
             List<string> guesses = new List<string> { };
             string guess = "a";
 
-            guesses = hm.AddToGuesses(guesses, guess);
+            guesses = hm.AddStringToList(guesses, guess);
 
             Assert.AreEqual(1, guesses.Count);
         }
 
         [TestMethod]
-        public void CalcLives_AllCorrectGuesses_ReturnsAllLivesRemaing()
+        public void RemoveLife_CorrectGuess_ReturnsFalse()
         {
             HangmanMain hm = MakeHangman();
             string word = "abc";
-            List<string> guesses = new List<string> { "a" };
-            int lives = 3;
+            string guess = "a";
 
-            Assert.AreEqual(3, hm.CalcLives(word, guesses, lives));
+            Assert.IsFalse(hm.RemoveLife(word, guess));
         }
 
         [TestMethod]
-        public void CalcLives_WrongGuesses_ReturnsCorrectLivesRemaing()
+        public void RemoveLife_InCorrectGuess_ReturnsTrue()
         {
             HangmanMain hm = MakeHangman();
             string word = "abc";
-            List<string> guesses = new List<string> { "d" , "f" };
-            int lives = 3;
+            string guess = "d";
 
-            Assert.AreEqual(1, hm.CalcLives(word, guesses, lives));
+            Assert.IsTrue(hm.RemoveLife(word, guess));
         }
 
         [TestMethod]
-        public void CalcLives_MixedGuesses_ReturnsCorrectLivesRemaing()
+        public void OutOfLives_PositiveLives_ReturnsFalse()
         {
             HangmanMain hm = MakeHangman();
-            string word = "abc";
-            List<string> guesses = new List<string> { "a" , "d" };
             int lives = 3;
 
-            Assert.AreEqual(2, hm.CalcLives(word, guesses, lives));
+            Assert.IsFalse(hm.OutOfLives(lives));
+        }
+
+        [TestMethod]
+        public void OutOfLives_ZeroLives_ReturnsTrue()
+        {
+            HangmanMain hm = MakeHangman();
+            int lives = 0;
+
+            Assert.IsTrue(hm.OutOfLives(lives));
+        }
+
+        [TestMethod]
+        public void WordGuessed_AllCorrect_ReturnsTrue()
+        {
+            HangmanMain hm = MakeHangman();
+
+            string word = "abc";
+            List<string> guesses = new List<string> { "a", "b", "c" };
+
+            Assert.IsTrue(hm.WordGuessed(word, guesses));
+        }
+
+        [TestMethod]
+        public void WordGuessed_NotAllCorrect_ReturnsFalse()
+        {
+            HangmanMain hm = MakeHangman();
+
+            string word = "abc";
+            List<string> guesses = new List<string> { "a", "b", "d" };
+
+            Assert.IsFalse(hm.WordGuessed(word, guesses));
+        }
+
+        [TestMethod]
+        public void FormatGuessesForPrinting_ReturnsCorrectString()
+        {
+            HangmanMain hm = MakeHangman();
+
+            List<string> guesses = new List<string> { "a", "b", "c" };
+
+            Assert.AreEqual("abc", hm.FormatGuessesForPrinting(guesses));
+        }
+
+        [TestMethod]
+        public void RoundNotFinished_WordGuessedAndLivesPositive_ReturnsFalse()
+        {
+            HangmanMain hm = MakeHangman();
+
+            int lives = 3;
+            string word = "abc";
+            List<string> guesses = new List<string> { "a", "b", "c" };
+
+            Assert.IsFalse(hm.RoundNotFinished(word, guesses, lives));
+        }
+
+        [TestMethod]
+        public void RoundNotFinished_WordNotGuessedAndLivesPositive_ReturnsTrue()
+        {
+            HangmanMain hm = MakeHangman();
+
+            int lives = 3;
+            string word = "abc";
+            List<string> guesses = new List<string> { "a", "b" };
+
+            Assert.IsTrue(hm.RoundNotFinished(word, guesses, lives));
+        }
+
+        [TestMethod]
+        public void RoundNotFinished_WordGuessedAndLivesZero_ReturnsFalse()
+        {
+            HangmanMain hm = MakeHangman();
+
+            int lives = 0;
+            string word = "abc";
+            List<string> guesses = new List<string> { "a", "b", "c" };
+
+            Assert.IsFalse(hm.RoundNotFinished(word, guesses, lives));
+        }
+
+        [TestMethod]
+        public void RoundNotFinished_WordNotGuessedAndLivesZero_ReturnsFalse()
+        {
+            HangmanMain hm = MakeHangman();
+
+            int lives = 0;
+            string word = "abc";
+            List<string> guesses = new List<string> { "a", "b" };
+
+            Assert.IsFalse(hm.RoundNotFinished(word, guesses, lives));
+        }
+
+        [TestMethod]
+        public void DuplicateGuess_NotDuplicate_ReturnsFalse()
+        {
+            HangmanMain hm = MakeHangman();
+
+            List<string> guesses = new List<string> { "a", "b" };
+            string guess = "c";
+
+            Assert.IsFalse(hm.DuplicateGuess(guesses, guess));
+        }
+
+        [TestMethod]
+        public void DuplicateGuess_Duplicate_ReturnsTrue()
+        {
+            HangmanMain hm = MakeHangman();
+
+            List<string> guesses = new List<string> { "a", "b" };
+            string guess = "a";
+
+            Assert.IsTrue(hm.DuplicateGuess(guesses, guess));
         }
     }
 }
